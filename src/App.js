53b67d817f38997ee04fb56c11d41401e16f10ad
Kaleb Koelle <kkoelle@student.fullsail.edu>
2025-10-15 20:09:04 -0400

    Update to Home / Landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ use HashRouter instead
 
-//CSS
+// CSS
 import "./global.css";
 // Components
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import About from "./components/About";
-// import Skills from "./components/Skills";
-// import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
